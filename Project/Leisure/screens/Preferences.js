95a93b10e70a33d0eef63c9c0f8bf7eefa4bc8e7
welbym <welbymoore0816@gmail.com>
2022-04-10 16:16:15 -0500

    Fixed menu

--- Project/Leisure/screens/Preferences.js
+++ Project/Leisure/screens/Preferences.js
@@ -6,23 +6,17 @@ const screenWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
 
 const Preferences = () => {
-    const [open, setOpen] = useState(false);
-    const [value, setValue] = useState(null);
-    const [items, setItems] = useState([
-        {label: 'Apple', value: 'apple'},
-        {label: 'Banana', value: 'banana'}
-    ]);
-
     return (
         <DropDownPicker
-        open={open}
-        value={value}
-        items={items}
-        setOpen={setOpen}
-        setValue={setValue}
-        setItems={setItems}
+            items={[
+                {label: 'Item 1', value: 'item1'},
+                {label: 'Item 2', value: 'item2'},
+            ]}
+            defaultIndex={0}
+            containerStyle={{height: 100, width: 100}}
+            onChangeItem={item => console.log(item.label, item.value)}
         />
-    );
+    )
 }
 
 export default Preferences
